1882221f7b6faadd536722feaecdd02c2731dd1c
Tjibbe van der Laan <tjibbevanderlaan@gmail.com>
2018-03-21 18:26:13 +0100

    Restructure the repo to building system webpack
    To use React as a front-end development framework, the webpack
    building engine is more suitable. Therefore, the building framework
    has been changed from bower/grunt to npm/webpack.

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const buildPath = path.resolve(__dirname, 'dist')
+
+const config = {
+  entry: {
+    main: [
+      './src/app/app.js',
+    ],
+  },
+  // Render source-map file for final build
+  devtool: 'source-map',
+  // output config
+  output: {
+    path: buildPath, // Path of output file
+    filename: 'app.js', // Name of output file
+  },
+  plugins: [
+    // Define production build to allow React to strip out unnecessary checks
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    // Transfer Files
+    new CopyWebpackPlugin([
+      { from: '_locales', to: '_locales' },
+      { from: 'icons', to: 'icons' },
+      { from: 'clang-newlib', to: 'clang-newlib'},
+      { from: 'app/controller/background.js', to: 'background.js'},
+      { from: 'app/model', to: 'model'},
+      { from: 'manifest.json', to: 'manifest.json'},
+      { from: 'window.html', to: 'window.html'}
+    ], {context: path.resolve(__dirname, 'src')}),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+        },
+      },
+    ],
+  },
+};
+
+module.exports = config;
